0b14d3e3316a8796cd7de889a811bf6b795c50a6
Jarek3 <jarek.patrny@gmail.com>
2022-12-11 02:55:51 +0100

    Created new seed to generate fake comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(TagsTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
     }
 }
