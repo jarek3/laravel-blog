03b0acba702526ed4c2681732c0b002b53f0e063
Jarek3 <jarek.patrny@gmail.com>
2023-01-18 03:23:50 +0100

    About

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,7 +28,7 @@
             <div class="collapse navbar-collapse" id="the-navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="{{url('home')}}">Blog</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="{{url('/home')}}">About</a></li>
                     <li><a href="{{url('backend/contact') }}">Contact</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
